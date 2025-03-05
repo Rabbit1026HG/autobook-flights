d3b6c88bdc06a432d9309a5ff7e3715ee451c059
rabbit <rabbit1026hg@gmail.com>
2025-03-06 06:58:04 +0900

    fix: home page update

--- src/shared/ui/Card/ReviewCard/ReviewCard.tsx
+++ src/shared/ui/Card/ReviewCard/ReviewCard.tsx
@@ -1,10 +1,10 @@
-import { FC } from 'react';
-import StarRow from '@shared/icons/24/star-unfilled.svg?react';
-import StarFilled from '@shared/icons/24/star-filled.svg?react';
-import StarHalf from '@shared/icons/24/star-half-filled.svg?react';
-import clsx from 'clsx';
-import styles from './ReviewCard.module.scss';
-import { Link } from 'react-router-dom';
+import { FC } from "react";
+import StarRow from "@shared/icons/24/star-unfilled.svg?react";
+import StarFilled from "@shared/icons/24/star-filled.svg?react";
+import StarHalf from "@shared/icons/24/star-half-filled.svg?react";
+import clsx from "clsx";
+import styles from "./ReviewCard.module.scss";
+import { Link } from "react-router-dom";
 interface ReviewCardProps extends React.HTMLAttributes<HTMLDivElement> {
   avatar: string;
   name: string;
@@ -26,33 +26,33 @@ export const ReviewCard: FC<ReviewCardProps> = ({
   ...other
 }) => {
   return (
-    <div {...other} className={clsx(styles['review-card'], className)}>
-      <div className={styles['avatar']}>
+    <div {...other} className={clsx(styles["review-card"], className)}>
+      <div className={styles["avatar"]}>
         <img src={avatar} alt={name} loading="lazy" />
       </div>
-      <div className={styles['review-content']}>
+      <div className={styles["review-content"]}>
         <h4>{name}</h4>
         <p>
           {address}
           <span> | </span>
           {date}
         </p>
-        <div className={styles['stars']}>
+        <div className={styles["stars"]}>
           {[...Array(5)].map((_, index) => {
             const starId = index + 1;
             if (starId <= stars) {
               return (
-                <StarFilled key={index} className={styles['star-filled']} />
+                <StarFilled key={index} className={styles["star-filled"]} />
               );
             }
             if (starId - stars < 1) {
-              return <StarHalf key={index} className={styles['star-half']} />;
+              return <StarHalf key={index} className={styles["star-half"]} />;
             }
-            return <StarRow key={index} className={styles['star-row']} />;
+            return <StarRow key={index} className={styles["star-row"]} />;
           })}
         </div>
-        <p className={styles['review']}>
-          {review.slice(0, 204)}{' '}
+        <p className={styles["review"]}>
+          {review.slice(0, 204)}{" "}
           {review.length > 170 && url && <Link to={url}>read more...</Link>}
         </p>
       </div>
